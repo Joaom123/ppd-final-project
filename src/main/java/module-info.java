870afe0e385552c0836bf01b191164f0fea23e5e
João Marcus <joaomarcus12769@gmail.com>
2022-06-19 15:14:33 -0300

    chore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,13 @@
 module ifce.ppd.finalproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.rmi;
+    requires jini.core;
+    requires jini.ext;
+
+    requires java.naming;
+    requires activemq.core;
+    requires javax.jms.api;
 
 
     opens ifce.ppd.finalproject to javafx.fxml;
