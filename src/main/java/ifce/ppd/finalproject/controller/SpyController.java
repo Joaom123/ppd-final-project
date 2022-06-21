4c18d17b3cf7d5fb8e8caac043d25c20b557e6cc
João Marcus <joaomarcus12769@gmail.com>
2022-06-21 13:22:56 -0300

    Finalizando SpyController

--- src/main/java/ifce/ppd/finalproject/controller/SpyController.java
+++ src/main/java/ifce/ppd/finalproject/controller/SpyController.java
@@ -10,6 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import net.jini.core.entry.UnusableEntryException;
 import net.jini.core.transaction.TransactionException;
 import net.jini.space.JavaSpace;
@@ -26,25 +27,30 @@ public class SpyController implements Initializable {
     public TextArea chat;
 
     @FXML
-    public ListView wordList;
+    public ListView<String> wordList = new ListView();
 
     @FXML
     public TextField newDangerousWordInput;
+
+    @FXML
+    public Text removeDangerousWordErrorText;
+
+    @FXML
+    public Text addDangerousWordErrorText;
 
     private JavaSpace javaSpaces = null;
 
     private final Set<Message> messageSet;
 
+    private final Set<String> dangerousWords;
+
     private Registry registry;
 
     private RMIInterface rmiInterfaceStub = null;
 
-    private void addMessageToChat(String author, String message) {
-        chat.appendText(author + ": " + message + "\n");
-    }
-
     public SpyController() throws RemoteException, NotBoundException {
         messageSet = new HashSet<>();
+        dangerousWords = new HashSet<>();
         System.out.println("Procurando serviço JavaSpace...");
 
         // Init Java Spaces connection
@@ -70,7 +76,13 @@ public class SpyController implements Initializable {
     }
 
     private boolean messageIsDangerous(Message message) {
-        return true;
+        String[] words = message.content.split("\\W+");
+
+        for (String word : words) {
+            if (dangerousWords.contains(word))
+                return true;
+        }
+        return false;
     }
 
     @Override
@@ -108,9 +120,42 @@ public class SpyController implements Initializable {
 
     @FXML
     public void addDangerousWord(ActionEvent actionEvent) {
+        String newWord = newDangerousWordInput.getText();
+
+        if(newWord == null || newWord.trim().isEmpty()) {
+            addDangerousWordErrorText.setText("Palavra Perigosa não pode ser vazia!");
+            return;
+        }
+
+        if(dangerousWords.contains(newWord)) {
+            addDangerousWordErrorText.setText("Palavra Perigosa já existe!");
+            return;
+        }
+
+        addDangerousWordErrorText.setText("");
+
+        // Add dangerous word to list
+        dangerousWords.add(newWord);
+        wordList.getItems().add(newWord);
     }
 
     @FXML
     public void removeDangerousWord(ActionEvent actionEvent) {
+        String selectedWord = wordList.getSelectionModel().getSelectedItem();
+
+        System.out.println(selectedWord);
+
+        if(selectedWord == null) {
+            removeDangerousWordErrorText.setText("Não há palavra selecionada!");
+            return;
+        }
+
+        removeDangerousWordErrorText.setText("");
+        dangerousWords.remove(selectedWord);
+        wordList.getItems().remove(selectedWord);
+    }
+
+    private void addMessageToChat(String author, String message) {
+        chat.appendText(author + ": " + message + "\n");
     }
 }
